5f98053b42af63e33a2eceba094553ac186402dc
Gonzalo <4588601+gonzacn@users.noreply.github.com>
2022-07-18 04:29:22 -0300

    Use open sans

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link
-                    href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,500;0,700;1,300;1,500&display=swap"
+                    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,500;0,700;1,300;1,500&display=swap"
                     rel="stylesheet"
                 />
 
