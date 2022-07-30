c74dce087d08dbbc06792356ed8a29e340a4a652
Gonzalo <4588601+z4gon@users.noreply.github.com>
2022-07-30 19:34:14 -0300

    Extract GA to component

---
+++ src/components/page/GoogleAnalytics.tsx
@@ -0,0 +1,25 @@
+import Script from 'next/Script'
+
+const GoogleAnalytics: React.FC<{}> = () => {
+    return (
+        <>
+            {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+            <Script
+                id="google-analytics-setup"
+                src="https://www.googletagmanager.com/gtag/js?id=G-NPBT98N1FD"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-NPBT98N1FD');
+                    `}
+            </Script>
+        </>
+    )
+}
+
+export default GoogleAnalytics
--- src/components/page/Page.tsx
+++ src/components/page/Page.tsx
@@ -1,26 +1,12 @@
 import { PropsWithChildren } from 'react'
-import Script from 'next/Script'
 import styles from '../../../styles/components/page/Page.module.sass'
 import NavBar from '../navbar/NavBar'
+import GoogleAnalytics from './GoogleAnalytics'
 
 const Page: React.FC<PropsWithChildren> = ({ children }) => {
     return (
         <div className={styles.page}>
-            {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-            <Script
-                src="https://www.googletagmanager.com/gtag/js?id=G-NPBT98N1FD"
-                strategy="beforeInteractive"
-            />
-            <Script id="google-analytics" strategy="beforeInteractive">
-                {`
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-
-                        gtag('config', 'G-NPBT98N1FD');
-                    `}
-            </Script>
-
+            <GoogleAnalytics />
             <NavBar />
             {children}
         </div>
