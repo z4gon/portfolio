f23ccd70176158a1609ed59c336e5e3b224d3c6a
Gonza C <4588601+z4gon@users.noreply.github.com>
2022-07-30 19:20:18 -0300

    Move analytics script to the page component (#14)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
     return (
@@ -22,21 +21,6 @@ export default function Document() {
                     type="text/css"
                     href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"
                 />
-
-                {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-                <Script
-                    src="https://www.googletagmanager.com/gtag/js?id=G-NPBT98N1FD"
-                    strategy="afterInteractive"
-                />
-                <Script id="google-analytics" strategy="afterInteractive">
-                    {`
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-
-                        gtag('config', 'G-NPBT98N1FD');
-                    `}
-                </Script>
             </Head>
             <body>
                 <Main />
