bf211f68e39daf82cbce1d9936905c13e142c691
z4gon <4588601+z4gon@users.noreply.github.com>
2022-09-26 23:15:46 -0300

    Fire VFX (#16)
    Implement portfolio entry

--- data/projects.ts
+++ data/projects.ts
@@ -1,8 +1,9 @@
 import { ProjectData } from '../src/models/ProjectData'
 
+import fireVfxUnity from './projects/fire-vfx-unity'
 import starcat from './projects/starcat'
 import waterShaderUnity from './projects/water-shader-unity'
 
-const projects: ProjectData[] = [waterShaderUnity, starcat]
+const projects: ProjectData[] = [fireVfxUnity, waterShaderUnity, starcat]
 
 export default projects
