5d68e4fed82c01c2a4b04ae92f3c4082be761242
Gonzalo <4588601+z4gon@users.noreply.github.com>
2022-09-28 15:02:18 -0300

    Implement portfolio entry

--- data/projects.ts
+++ data/projects.ts
@@ -1,9 +1,18 @@
 import { ProjectData } from '../src/models/ProjectData'
 
 import fireVfxUnity from './projects/fire-vfx-unity'
+import iceAttackVfxUnity from './projects/ice-attack-vfx-unity'
 import starcat from './projects/starcat'
 import waterShaderUnity from './projects/water-shader-unity'
 
-const projects: ProjectData[] = [fireVfxUnity, waterShaderUnity, starcat]
+const pinned: ProjectData[] = [
+    iceAttackVfxUnity,
+    fireVfxUnity,
+    waterShaderUnity,
+]
+const publishedGames: ProjectData[] = [starcat]
+const other: ProjectData[] = []
+
+const projects = [...pinned, ...publishedGames, ...other]
 
 export default projects
