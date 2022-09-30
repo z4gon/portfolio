3ab4dabf90ecc2e8f04b9c1a40ef508a6fb32659
Gonzalo <4588601+z4gon@users.noreply.github.com>
2022-09-30 15:04:25 -0300

    Portfolio entry

--- data/projects.ts
+++ data/projects.ts
@@ -1,11 +1,13 @@
 import { ProjectData } from '../src/models/ProjectData'
 
 import fireVfxUnity from './projects/fire-vfx-unity'
+import groundSlashVfxUnity from './projects/ground-slash-vfx-unity'
 import iceAttackVfxUnity from './projects/ice-attack-vfx-unity'
 import starcat from './projects/starcat'
 import waterShaderUnity from './projects/water-shader-unity'
 
 const pinned: ProjectData[] = [
+    groundSlashVfxUnity,
     iceAttackVfxUnity,
     fireVfxUnity,
     waterShaderUnity,
