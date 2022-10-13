73c55be97469ae4688316e33fcc786258b433a47
z4gon <4588601+z4gon@users.noreply.github.com>
2022-10-13 19:46:55 -0300

    Typos (#29)
    * Ball of lava vfx project entry
    
    * Typos

--- data/projects/ball-of-lava-shader-unity.ts
+++ data/projects/ball-of-lava-shader-unity.ts
@@ -22,11 +22,11 @@ const data: ProjectData = generateProjectData({
         'Ball of Lava VFX written in the Cg programming language for Unity Built-In RP.',
     ],
     implementationDetails: [
-        'Using the Perlin Noise algorithm to displace verteces in the mesh, during the Vertex Shader Stage.',
-        'Use _Time to animate the Perlin Noise, also multiply by _Displacement and _NoiseVelocity to control how much and how fast the noise is.',
-        'Displace the uv coordinates to simulate rotation, multiply by _RotationVelocity to control the speed.',
-        "Generate a Mesh and make sure the UV map wraps correctly, so the Perlin noise doesn't generate weird shapes in the mesh.",
-        'Lerp between colors using steps.',
+        'Using the Perlin Noise algorithm to displace vertices in the mesh, during the Vertex Shader Stage.',
+        'Using _Time to animate the Perlin Noise, also multiplying by _Displacement and _NoiseVelocity to control how much and how fast the noise is.',
+        'Displacing the uv coordinates to simulate rotation, multiplying by _RotationVelocity to control the speed.',
+        "Generating a Mesh and making sure the UV map wraps correctly, so the Perlin noise doesn't generate weird shapes in the mesh.",
+        'Lerping between colors using steps.',
     ],
     tags: [
         Tag.Cg,
