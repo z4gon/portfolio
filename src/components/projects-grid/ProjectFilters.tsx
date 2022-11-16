9ca4b8b267036733857b25d6da96c2f559a7958e
Gonzalo <4588601+z4gon@users.noreply.github.com>
2022-11-16 14:39:04 -0300

    filter

--- src/components/projects-grid/ProjectFilters.tsx
+++ src/components/projects-grid/ProjectFilters.tsx
@@ -1,32 +1,62 @@
+import { useEffect, useState } from 'react'
 import { includes, filter } from 'lodash'
 import styles from '../../../styles/components/projects-grid/ProjectFilters.module.sass'
 import { ProjectDataMinimal } from '../../models/ProjectData'
+import SearchIcon from '../icons/SearchIcon'
 
 interface ProjectFiltersProps {
     allProjects: ProjectDataMinimal[]
-    onFiltersChanged?: (filterResults: ProjectDataMinimal[]) => void
+    onFiltersChanged: (filterResults: ProjectDataMinimal[]) => void
 }
 
 const ProjectFilters: React.FC<ProjectFiltersProps> = ({
     allProjects,
     onFiltersChanged,
 }) => {
-    const onFilter = (searchInput: string) => {
-        const filterResults = filter(allProjects, (p) =>
-            includes(p.title.toLowerCase(), searchInput.toLowerCase())
-        )
+    const [searchInput, setSearchInput] = useState('')
+    const [filterResultsCount, setFilterResoultsCount] = useState(
+        allProjects.length
+    )
 
+    const onFilter = () => {
+        const filterResults: ProjectDataMinimal[] = searchInput
+            ? filter(allProjects, (project) =>
+                  includes(
+                      project.title.toLowerCase(),
+                      searchInput.toLowerCase()
+                  )
+              )
+            : allProjects
+
+        setFilterResoultsCount(filterResults.length)
         onFiltersChanged(filterResults)
+    }
+
+    useEffect(() => {
+        onFilter()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [searchInput])
+
+    const onSearchChange = (searchInput: string) => {
+        setSearchInput(searchInput)
+        onFilter()
     }
 
     return (
         <div className={styles.filters}>
-            <div>
+            <div className={styles.searchBox}>
                 <input
                     type="text"
-                    onChange={(e) => onFilter(e.currentTarget.value)}
+                    placeholder="Search for projects"
+                    onChange={(e) => onSearchChange(e.currentTarget.value)}
                 />
+                <SearchIcon />
             </div>
+            <span className={styles.message}>
+                {searchInput
+                    ? `${filterResultsCount} projects matched search criteria`
+                    : `${filterResultsCount} available projects`}
+            </span>
         </div>
     )
 }
