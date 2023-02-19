371cfc58592ea671b57ea8533a573db822c4b912
z4gon <4588601+z4gon@users.noreply.github.com>
2023-02-19 15:20:49 -0300

    yaml generator

---
+++ data/getProjectFrontMatter.ts
@@ -0,0 +1,34 @@
+import { Category } from '../src/models/enums/Category'
+import { Tag } from '../src/models/enums/Tag'
+import { Technology } from '../src/models/enums/Technology'
+import { LinkData } from '../src/models/LinkData'
+
+export interface ProjectDefinition {
+    id: string
+    aliases?: string[]
+    imagesNames?: string[]
+    videosNames?: string[]
+    date: string
+    youtubeVideoIds?: string[]
+    primaryImageName?: string
+    primaryVideoName?: string
+    title: string
+    subtitle: string
+    description?: string[]
+    implementationDetails: string[]
+    tags: Tag[]
+    gitHubUrl?: string
+    links?: LinkData[]
+    appleAppStoreUrl?: string
+    googlePlayStoreUrl?: string
+    isPrivateRepo?: boolean
+    technology?: Technology
+    category: Category
+}
+
+const getProjectFrontMatter = (projectDefinition: ProjectDefinition) => `
+---
+---
+`
+
+export default getProjectFrontMatter
