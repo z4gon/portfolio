d2884262de26e3d267e042f40b994b1e237a9d7e
z4gon <4588601+z4gon@users.noreply.github.com>
2023-02-19 21:05:00 -0300

    fix

--- data/getProjectFrontMatter.ts
+++ data/getProjectFrontMatter.ts
@@ -7,55 +7,55 @@ import { Technology } from '../src/models/enums/Technology'
 import { LinkData } from '../src/models/LinkData'
 
 export interface ProjectDefinition {
-  id: string
-  aliases?: string[]
-  imagesNames?: string[]
-  videosNames?: string[]
-  date: string
-  youtubeVideoIds?: string[]
-  primaryImageName?: string
-  primaryVideoName?: string
-  title: string
-  subtitle: string
-  description?: string[]
-  implementationDetails: string[]
-  tags: Tag[]
-  gitHubUrl?: string
-  links?: LinkData[]
-  appleAppStoreUrl?: string
-  googlePlayStoreUrl?: string
-  isPrivateRepo?: boolean
-  technology?: Technology
-  category: Category
+	id: string
+	aliases?: string[]
+	imagesNames?: string[]
+	videosNames?: string[]
+	date: string
+	youtubeVideoIds?: string[]
+	primaryImageName?: string
+	primaryVideoName?: string
+	title: string
+	subtitle: string
+	description?: string[]
+	implementationDetails: string[]
+	tags: Tag[]
+	gitHubUrl?: string
+	links?: LinkData[]
+	appleAppStoreUrl?: string
+	googlePlayStoreUrl?: string
+	isPrivateRepo?: boolean
+	technology?: Technology
+	category: Category
 }
 
 // the weird new lines are intentional
 const getProjectFrontMatter = (projectDefinition: ProjectDefinition) => {
-  const { id, ...rest } = projectDefinition
-  return `---
+	const { id, ...rest } = projectDefinition
+	return `---
 ${yaml.stringify(rest)}---`
 }
 
 // generateAllFrontMatterMdFiles(projects, 'data/projects/')
 export const generateAllFrontMatterMdFiles = (
-  projects: ProjectDefinition[],
-  targetDirectory: string
+	projects: ProjectDefinition[],
+	targetDirectory: string
 ) => {
-  const __dirname = join(process.cwd(), targetDirectory)
-  projects.forEach((project) => {
-    const { id, ...rest } = project
-    fs.appendFile(
-      __dirname + `${id}.md`,
-      getProjectFrontMatter(project),
-      function (err) {
-        if (err) {
-          // append failed
-        } else {
-          // done
-        }
-      }
-    )
-  })
+	const __dirname = join(process.cwd(), targetDirectory)
+	projects.forEach((project) => {
+		const { id, ...rest } = project
+		fs.appendFile(
+			__dirname + `${id}.md`,
+			getProjectFrontMatter(project),
+			function (err) {
+				if (err) {
+					// append failed
+				} else {
+					// done
+				}
+			}
+		)
+	})
 }
 
 export default getProjectFrontMatter
